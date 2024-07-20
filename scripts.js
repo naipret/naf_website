de4c9bfd1aff5c6fae13adadf33fec10811d380c
Hjeyki <hungnk209@gmail.com>
2024-07-20 07:39:13 +0700

    Sửa lại thanh taskbar

--- scripts.js
+++ scripts.js
@@ -1,9 +1,20 @@
-function showSection(sectionId) {
+function showSection(sectionId, button) {
+    // Ẩn tất cả các phần nội dung
     const sections = document.querySelectorAll('.section');
     sections.forEach(section => {
         section.style.display = 'none';
     });
-    
+
+    // Hiển thị phần nội dung được chọn
     const activeSection = document.getElementById(sectionId);
     activeSection.style.display = 'block';
+
+    // Xóa lớp 'active' từ tất cả các nút
+    const buttons = document.querySelectorAll('.naf-button');
+    buttons.forEach(btn => {
+        btn.classList.remove('active');
+    });
+
+    // Thêm lớp 'active' vào nút được chọn
+    button.classList.add('active');
 }
