9c4eb1a70b3772fc684ef7b6fd26ff4b85280752
NaiPret <ngvphu235e@gmail.com>
2024-07-21 10:02:29 +0700

    main <-- web-3

--- scripts.js
+++ scripts.js
@@ -1,20 +1,48 @@
-function showSection(sectionId, button) {
-    // Ẩn tất cả các phần nội dung
-    const sections = document.querySelectorAll('.section');
-    sections.forEach(section => {
-        section.style.display = 'none';
+function showContent(index) {
+    // Hide all content items
+    const contentItems = document.querySelectorAll('.content-item');
+    contentItems.forEach(item => {
+        item.style.display = 'none';
     });
 
-    // Hiển thị phần nội dung được chọn
-    const activeSection = document.getElementById(sectionId);
-    activeSection.style.display = 'inline';
+    // Show the selected content item
+    const selectedContent = document.getElementById(`content${index}`);
+    selectedContent.style.display = 'block';
+}
 
-    // Xóa lớp 'active' từ tất cả các nút
-    const buttons = document.querySelectorAll('.naf-button');
-    buttons.forEach(btn => {
-        btn.classList.remove('active');
-    });
+function toggleTaskbar() {
+    const taskbar = document.getElementById('taskbar');
+    const showButton = document.getElementById('show-taskbar-btn');
+    const content = document.querySelector('.content');
+    const isTaskbarHidden = taskbar.style.transform === 'translateX(-100%)';
 
-    // Thêm lớp 'active' vào nút được chọn
-    button.classList.add('active');
+    if (isTaskbarHidden) {
+        taskbar.style.transform = 'translateX(0)';
+        showButton.style.display = 'none';
+        content.style.marginLeft = 'calc(200px + 3dvh)'; // Điều chỉnh margin để tạo khoảng cách cho taskbar cộng với 3dvh
+    } else {
+        taskbar.style.transform = 'translateX(-100%)';
+        showButton.style.display = 'block';
+        content.style.marginLeft = '3dvh'; // Đảm bảo margin là 3dvh khi taskbar bị ẩn
+    }
+}
+
+// Đảm bảo taskbar ban đầu được hiển thị
+document.getElementById('taskbar').style.transform = 'translateX(0)';
+
+// Đảm bảo nội dung cách thanh taskbar một khoảng 3dvh khi trang mới tải
+document.querySelector('.content').style.marginLeft = '30dvh';
+
+function copyText() {
+    const textToCopy = 'naf.com';
+    const tempInput = document.createElement('input');
+    tempInput.style.position = 'absolute';
+    tempInput.style.left = '-9999px';
+    tempInput.value = textToCopy;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    tempInput.setSelectionRange(0, 99999); // Cho các thiết bị di động
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+    alert('Copied: ' + textToCopy);
 }
